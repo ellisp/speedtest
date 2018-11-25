2391a8f0f1581ff52b008d7de678f257f5fb0261
Peter Ellis <peter.ellis2013nz@gmail.com>
2018-11-25 01:32:44 +0000

    add other variables to shiny app

--- shiny/server.R
+++ shiny/server.R
@@ -2,13 +2,28 @@ library(shiny)
 library(dygraphs)
 library(readr)
 library(xts)
+library(GGally)
+
 Sys.setenv(TZ = "Australia/Sydney")
 
 
 res <- read_csv("https://github.com/ellisp/speedtest/raw/master/test-results.csv",
                 locale = locale(tz = "Australia/Sydney"))
-dl <- xts(res$download / 1000000, as.POSIXct(res$timestamp), tzone = "Australia/Sydney")
+res$download <- res$download / 1000000
+res$upload <- res$upload / 1000000
 
+dl <- xts(res$download, as.POSIXct(res$timestamp), tzone = "Australia/Sydney")
+ul <- xts(res$upload, as.POSIXct(res$timestamp), tzone = "Australia/Sydney")
+ping <- xts(res$ping, as.POSIXct(res$timestamp), tzone = "Australia/Sydney")
+
+#' Function for drawing a path plot
+csp <- function(data, mapping, ...){
+  p <- ggplot(data = data, mapping = mapping) +
+    geom_path(colour = "grey50") +
+    geom_point(colour = "steelblue")
+    
+  return(p)
+}
 
 shinyServer(function(input, output, session) {
    output$download <- renderDygraph({
@@ -16,5 +31,24 @@ shinyServer(function(input, output, session) {
        dyAxis("y", label = "Megabits per second\n") %>%
        dyRangeSelector()
    })
+   
+   output$upload <- renderDygraph({
+     dygraph(ul, main = "Upload rates") %>%
+       dyAxis("y", label = "Megabits per second\n") %>%
+       dyRangeSelector()
+   })
+   
+   output$ping <- renderDygraph({
+     dygraph(ping, main = "Latency time") %>%
+       dyAxis("y", label = "Milliseconds\n") %>%
+       dyRangeSelector()
+   })
+   
+   output$pairs <- renderPlot({
+     res[ , 1:3] %>%
+       ggpairs(lower = list(continuous = csp)) +
+       theme_bw() +
+       theme(strip.background = element_rect(colour = "grey85"))
+     })
 
 })
--- shiny/ui.R
+++ shiny/ui.R
@@ -4,6 +4,13 @@ library(dygraphs)
 shinyUI(fluidPage(
   
   titlePanel("Internet speed in Fitzroy, Melbourne"),
-   dygraphOutput("download")
+  tabsetPanel(
+    tabPanel("Download speed", dygraphOutput("download")),
+    tabPanel("Upload speed", dygraphOutput("upload")),
+    tabPanel("Latency time", dygraphOutput("ping")),
+    tabPanel("Correlations", plotOutput("pairs", height = "600px"))
+    
+  ) 
+  
 
 ))
